cd332307cb155e0c588620854ed6cddcb324286c
TianYuan <95566315+ty01528@users.noreply.github.com>
2022-08-28 00:32:50 +0800

    删除测试用例

--- interact/depositFromFile.go
+++ interact/depositFromFile.go
@@ -25,10 +25,8 @@ func DepositFromFile(addr string) {
 		if c == io.EOF {
 			break
 		}
-		//addressRaw := "0xe4bbaf48d0202dc4209b09c7b173cd7e9aee669c"
 		addressSub := string(addressRaw)[2:]
 		addressSub = strings.Replace(addressSub, " ", "", -1)
-		//address = 0xB2174c3Cb47bC9E417908905B7F8D65d06f4140c
 		res := Deposit(addressSub)
 		time.Sleep(1 * time.Second)
 		_, _ = fmt.Fprintln(logsFile, res)
