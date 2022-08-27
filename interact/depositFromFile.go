553dc4d251eb4056ef4abd520ce607c593beb0f3
TianYuan <tianyuan01528@foxmail.com>
2022-08-27 23:37:24 +0800

    初始提交

---
+++ interact/depositFromFile.go
@@ -0,0 +1,41 @@
+package interact
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/big"
+	"os"
+	"strings"
+	"time"
+)
+
+func DepositFromFile(addr string) {
+	logsName := fmt.Sprint("logs", time.Now().UnixNano(), ".txt")
+	logsFile, _ := os.OpenFile(logsName, os.O_RDWR|os.O_CREATE, os.ModeAppend)
+	addrListFile, err := os.Open(addr)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer addrListFile.Close()
+
+	addrList := bufio.NewReader(addrListFile)
+	for {
+		addressRaw, _, c := addrList.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		//addressRaw := "0xe4bbaf48d0202dc4209b09c7b173cd7e9aee669c"
+		addressSub := string(addressRaw)[2:]
+		addressSub = strings.Replace(addressSub, " ", "", -1)
+		address, _ := new(big.Int).SetString(addressSub, 16)
+		//address = 0xB2174c3Cb47bC9E417908905B7F8D65d06f4140c
+		res := Deposit(address)
+		time.Sleep(1 * time.Second)
+		_, _ = fmt.Fprintln(logsFile, res)
+		//if err != nil {
+		//	return
+		//}
+	}
+}
